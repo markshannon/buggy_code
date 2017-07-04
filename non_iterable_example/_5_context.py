83db8d5eb376304b1482b3f46f0b6a800571f50c
Mark Shannon <mark@semmle.com>
2017-07-04 13:11:38 +0100

    Make function exist when called.

--- non_iterable_example/_5_context.py
+++ non_iterable_example/_5_context.py
@@ -1,13 +1,13 @@
 
+def print_numbers(numbers):
+    for n in numbers:
+        print(n)
+
 if random:
     numbers = 1
     print_numbers(numbers)
 else:
     numbers = 1, 2, 3
     print_numbers(numbers)
-
-def print_numbers(numbers):
-    for n in numbers:
-        print(n)
 
 
