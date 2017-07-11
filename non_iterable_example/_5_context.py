2430d4ae362ca22ebff83b405355d60343b3a0c1
Mark Shannon <mark@hotpy.org>
2017-07-11 13:35:51 +0100

    Modify example to emphasise importance of context.

--- non_iterable_example/_5_context.py
+++ non_iterable_example/_5_context.py
@@ -1,13 +1,15 @@
 
-def print_numbers(numbers):
-    for n in numbers:
-        print(n)
+def print_numbers(flag, numbers):
+    if flag:
+        for n in numbers:
+            print(n)
 
 if random:
     numbers = 1
-    print_numbers(numbers)
+    print_numbers(False, numbers)
 else:
     numbers = 1, 2, 3
-    print_numbers(numbers)
+    print_numbers(True, numbers)
+
 
 
